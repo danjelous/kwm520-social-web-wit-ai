e3b52adba57e97746bca32b56fd6dfb3a9314c95
Danjelous <daniel.brunner@live.at>
2017-06-23 11:04:05 +0200

    Get weather from openweathermap from intents

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const {Wit} = require('node-wit');
+const { Wit } = require('node-wit');
+const request = require('request');
 
 const Client = new Wit({
     accessToken: '6AJQKKLX2ZIN764W2G7IVFN7SALSENW7'
 });
 
-Client.message('Gruezi', {})
+Client.message('Wie ist das Wetter in Linz?', {})
     .then((data) => {
-        let intent = data.entities.intent[0].value;
-        console.log(intent);
+        let intent = data.entities.intent[0].value;;
 
-        switch(intent) {
+        switch (intent) {
             case 'Greeting':
                 console.log('Hallo, freut mich dass du den KWM Bot benutzt!');
                 break;
             case 'Weather':
-                console.log('Wetter erkannt');
+                let location = data.entities.location[0].value;
+                getWeather(location);
                 break;
             case 'Routing':
                 console.log('Routing erkannt');
@@ -24,3 +25,18 @@ Client.message('Gruezi', {})
                 break;
         }
     });
+
+getWeather = (location) => {
+    request({
+        uri: 'http://api.openweathermap.org/data/2.5/weather?q=' +
+        location + '&APPID=6d27a14750ed96c42de990ad1f236ec2',
+        method: 'GET'
+    }, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            console.log(body);
+        } else {
+            console.log('Failed calling API', res.statusCode,
+                res.statusMessage, body.error);
+        }
+    });
+}
